940d56806d5987c7aa611823eae1ce130e39fc99
Johann Jungbauer <jungbauer@gmail.com>
2025-11-12 09:23:05 +0200

    build: adds custom EsLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,16 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from "eslint-config-vuetify";
 
-export default vuetify()
+export default vuetify(
+  {},
+  {
+    rules: {
+      "@stylistic/space-before-function-paren": ["error", {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+      }],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/arrow-parens": ["error", "always"],
+    },
+  });
