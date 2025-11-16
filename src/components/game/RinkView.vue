615bf7f35d5bd12c55d92a1d1e29edc35c186f05
Johann Jungbauer <jungbauer@gmail.com>
2025-11-16 11:54:50 +0200

    feat: makes RinkView responsive to element width

--- src/components/game/RinkView.vue
+++ src/components/game/RinkView.vue
@@ -1,44 +1,38 @@
 <script setup>
-  import { useDisplay } from "vuetify/framework";
   import useRinkDraw from "@/utils/useRinkDraw.js";
 
   const props = defineProps(["goals"]);
-
-  const { mobile, name, height, width } = useDisplay();
   const [drawRink, lengthToWidthRatio] = useRinkDraw();
 
   const elementRef = ref(null);
 
   onMounted(() => {
     let portraitDraw = false;
-    if (elementRef.value.offsetWidth < 1000) {
-      console.log("elw:", elementRef.value.offsetWidth);
-      portraitDraw = true;
-    }
-
     const canvas = document.querySelector("#rink");
     canvas.width = 1000;
-    canvas.height = canvas.width / lengthToWidthRatio();
+    canvas.height = canvas.width / lengthToWidthRatio(); // 425px
     // 140px : 1000px => 0.14 : 1
     let borderRadius = canvas.width * 0.14;
 
-    if (portraitDraw) {
-      canvas.height = 1000;
-      canvas.width = canvas.height / lengthToWidthRatio();
+    if (elementRef.value.offsetWidth < 1000) {
+      portraitDraw = true;
+      if (elementRef.value.offsetWidth >= 425) {
+        canvas.height = 1000;
+        canvas.width = canvas.height / lengthToWidthRatio(); // 425px
+      } else {
+        // screen width less than 425px reference
+        canvas.width = elementRef.value.offsetWidth;
+        canvas.height = canvas.width * lengthToWidthRatio();
+      }
       borderRadius = canvas.height * 0.14;
     }
 
     canvas.style.borderRadius = borderRadius.toString() + "px";
     drawRink(canvas, props.goals, portraitDraw);
   });
-
-  const widthTest = computed(() => {
-    return width.value;
-  });
 </script>
 
 <template>
-  <div v-if="elementRef">elw: {{ elementRef.offsetWidth }}</div>
   <div id="rink-container" ref="elementRef">
     <canvas id="rink" />
   </div>
