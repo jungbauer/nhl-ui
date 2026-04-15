6db83d74f3de6abd0cbe0717a57bc86c4bac5a47
Johann Jungbauer <jungbauer@gmail.com>
2026-04-15 15:22:58 +0200

    feat: uses date strings from GamesAroundToday api

--- src/components/AroundTodayView.vue
+++ src/components/AroundTodayView.vue
@@ -8,6 +8,9 @@
   const yesterdayGames = ref([]);
   const todayGames = ref([]);
   const tomorrowGames = ref([]);
+  const yesterdayStr = ref("");
+  const todayStr = ref("");
+  const tomorrowStr = ref("");
   const panels = ref(["yesterday"]);
 
   const teamScheduleUrl = `${import.meta.env.VITE_API_URL}/nhl/games/around-today`;
@@ -46,6 +49,10 @@
     for (const game of aroundData.tomorrow) {
       tomorrowGames.value.push(mapApiGame(game));
     }
+
+    yesterdayStr.value = aroundData.yesterdayDate;
+    todayStr.value = aroundData.todayDate;
+    tomorrowStr.value = aroundData.tomorrowDate;
   });
 </script>
 
@@ -56,17 +63,17 @@
       <h3>Games around today for 2025-2026 season</h3>
 
       <v-expansion-panels v-model="panels" multiple>
-        <v-expansion-panel :title="'Yesterday\'s Games (' + yesterdayGames.length + ')'" value="yesterday">
+        <v-expansion-panel :title="'Yesterday\'s Games (' + yesterdayGames.length + ') ' + yesterdayStr" value="yesterday">
           <v-expansion-panel-text>
             <AroundTodayGame v-for="(game, i) in yesterdayGames" :key="'yest' + i" :game="game" />
           </v-expansion-panel-text>
         </v-expansion-panel>
-        <v-expansion-panel :title="'Today\'s Games (' + todayGames.length + ')'" value="today">
+        <v-expansion-panel :title="'Today\'s Games (' + todayGames.length + ') ' + todayStr" value="today">
           <v-expansion-panel-text>
             <AroundTodayGame v-for="(game, i) in todayGames" :key="'today' + i" :game="game" />
           </v-expansion-panel-text>
         </v-expansion-panel>
-        <v-expansion-panel :title="'Tomorrow\'s Games (' + tomorrowGames.length + ')'" value="tomorrow">
+        <v-expansion-panel :title="'Tomorrow\'s Games (' + tomorrowGames.length + ') ' + tomorrowStr" value="tomorrow">
           <v-expansion-panel-text>
             <AroundTodayGame v-for="(game, i) in tomorrowGames" :key="'tomo' + i" :game="game" />
           </v-expansion-panel-text>
